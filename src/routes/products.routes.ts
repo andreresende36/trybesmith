2b67af4a6ad2b2b8e9f44cf94b6354752b83b6a5
André Resende <andreresende36@gmail.com>
2023-05-30 21:27:45 -0300

    feature 5. No tests implemented

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import productsController from '../controllers/products.controllers';
+import nameValidation from '../middlewares/nameValidation';
+import priceValidation from '../middlewares/priceValidation';
 
 const router = Router();
 
-router.post('/', productsController.create);
+router.post('/', nameValidation, priceValidation, productsController.create);
 router.get('/', productsController.getAll);
 
 export default router;
