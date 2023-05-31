2eb152b31a99e63fa49ae7fcd886aad12494d1ec
André Resende <andreresende36@gmail.com>
2023-05-31 18:54:41 -0300

    fix lint

--- src/test.js
+++
@@ -1,9 +0,0 @@
-import jwtUtils from './utils/jwtUtils';
-
-const bcrypt = require('bcryptjs');
-
-// bcrypt.hash('terrível', 0).then((data) => console.log(data));
-
-// bcrypt.compare('terrível', '$2a$10$HQL/MfRFEnytZbjjUFJZFuzuUPIeV1wRRAYnI7iFWL9/l9rO1qU7e').then((data) => console.log(data));
-
-// $2a$10$vTSX/aJN.fWK4p.QR6jUJehKus7AtCNHQByRPdSpHY1079FAILjkK
